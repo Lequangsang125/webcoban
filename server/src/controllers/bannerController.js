570f548bcf34e4d1dcfe4e026237d9e9f9ecddf8
Le Quang Sang <sanglq125@gmail.com>
2025-03-01 22:15:44 +0700

    .

---
+++ server/src/controllers/bannerController.js
@@ -0,0 +1,9 @@
+export const getAllBanners = async(req,res) =>{
+    try {
+        const 
+    } catch (error) {
+        return res.status(500).json({
+            message:error.message
+        })
+    }
+}
---
+++ server/src/models/Banner.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+const BannerSchema = new mongoose.Schema(
+    {
+        title: {type:String,required:true,trim:true},
+        image:{typ}
+    }
+)
